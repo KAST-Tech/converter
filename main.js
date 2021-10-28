214ee579b2911fee8998dcd3cef2c12ac38dd759
Monmoy042 <monmoykms.16@gmail.com>
2021-10-28 19:32:07 +0600

    Inch to centimeter & centimeter to inch added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Kilometer to mile conversion
 const kiloMileConvertBtn = document.getElementById('convertBtn1');
 kiloMileConvertBtn.addEventListener('click', function () {
 	let inputValue = getInput('kiloInput');
@@ -7,6 +8,7 @@ kiloMileConvertBtn.addEventListener('click', function () {
 	refresh('kiloInput');
 });
 
+// Mile to kilometer conversion
 const mileKiloConvertBtn = document.getElementById('convertBtn2');
 mileKiloConvertBtn.addEventListener('click', function () {
 	let inputValue = getInput('mileInput');
@@ -16,14 +18,37 @@ mileKiloConvertBtn.addEventListener('click', function () {
 	refresh('mileInput');
 });
 
+// Inch to centimeter conversion
+const inchCentimeterConvertBtn = document.getElementById('convertBtn3');
+inchCentimeterConvertBtn.addEventListener('click', function () {
+	let inputValue = getInput('inchInput');
+	let centimeter = inputValue * 2.54;
+	showConvertedArea('inch-cent-done');
+	document.getElementById('show-result3').innerText = centimeter;
+	refresh('inchInput');
+});
+
+// Centimeter to inch conversion
+const centimeterInchConvertBtn = document.getElementById('convertBtn4');
+centimeterInchConvertBtn.addEventListener('click', function () {
+	let inputValue = getInput('centInput');
+	let centimeter = inputValue * 0.393701;
+	showConvertedArea('cent-inch-done');
+	document.getElementById('show-result4').innerText = centimeter;
+	refresh('centInput');
+});
+
+// Input function
 function getInput(id) {
 	const input = document.getElementById(id).value;
 	const inputValue = parseFloat(input);
 	return inputValue;
 }
+// display:block output div
 function showConvertedArea(id) {
 	document.getElementById(id).style.display = 'block';
 }
+// Refresh the input section
 function refresh(id) {
 	document.getElementById(id).value = '';
 }
